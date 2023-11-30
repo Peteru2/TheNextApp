fa45dc8ce31374189bc19bfc566174da5ca7fb82
DevPeter <polalekan526@gmail.com>
2023-11-30 18:46:42 +0100

    New pages added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-
-import About from './about'
+// import 
+// import About from './pages/about'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h2>Jesus is Lord</h2>
-     <About />
+     <h2>Jesus is Lord above all</h2>
+     {/* <About /> */}
     </main>
   )
 }
