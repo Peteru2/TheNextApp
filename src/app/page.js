d4acd2622034e6cdace6425507444dbac1afbfd6
DevPeter <polalekan526@gmail.com>
2023-12-04 23:49:38 +0100

    Let's push the slight change

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-// import About from './pages/about'
+import About from './about/page'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h2>Jesus is Lord above all</h2>
-     {/* <About /> */}
+     <About />
     </main>
   )
 }
