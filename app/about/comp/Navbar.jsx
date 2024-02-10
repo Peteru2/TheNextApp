a1e537b57d24550e12089682521d43f3ba3b428d
Olatunji Peter <polalekan526@gmail.com>
2024-02-10 19:06:06 +0100

    Navbar included

---
+++ app/about/comp/Navbar.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../../public/next.jpg"
+const Navbar = () => {
+    return ( 
+        <nav>
+            <Image 
+              src={Logo}
+              width={40}
+              alt="Logo"
+              placeholder="blur"
+            />
+            <h2>TheNextApp</h2>
+            <Link href='/'>Dashboard</Link>
+            <Link href='/tickets'>Tickets</Link>
+        </nav>
+     );
+}
+ 
+export default Navbar;
