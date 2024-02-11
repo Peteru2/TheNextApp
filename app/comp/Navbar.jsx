6048f1d148e1c3ba1f34285965239f64350a2136
Olatunji Peter <polalekan526@gmail.com>
2024-02-11 20:51:32 +0100

    Ticket data added

--- app/comp/Navbar.jsx
+++ app/comp/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import Logo from "../../../public/next.jpg"
+import Logo from "../../public/next.jpg"
 const Navbar = () => {
     return ( 
         <nav>
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./about/comp/Navbar";
+import Navbar from "./comp/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
