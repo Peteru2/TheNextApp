90e1bb5766908fee0d509757310118d5326faa21
Olatunji Peter <polalekan526@gmail.com>
2024-02-12 16:02:08 +0100

    not found page added

---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+    return ( 
+        <main className="text-center">
+            <h3>Loading..</h3>
+            <p>Wait a little nit :)</p>
+        </main>
+     );
+}
+ 
+export default Loading;
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 const NotFound = () => {
     return ( 
-        <main>
-            
+        <main className="text-center ">
+                    <h2>There was a problem</h2>
+                    <p>We could not fetch the page you requested for </p>
+                    <p>Go back to the <Link href="/">Dashboard</Link></p>
         </main>
      );
 }
